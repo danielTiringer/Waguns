192a96a3fd73bd85372bb6f068df71ea168bd0f1
Galicz555 <galicz555@gmail.com>
2020-02-20 13:34:12 +0100

    firstLooks

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { StatsComponent } from './components/stats/stats.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { StatsComponent } from "./components/stats/stats.component";
+import { NotFoundComponent } from "../app/components/not-found/not-found.component";
+import { ReeentACarComponent } from "../app/components/reeent-a-car/reeent-a-car.component";
 import { LoginPageComponent } from './components/login-page/login-page.component';
 
-
 const routes: Routes = [
-  {path: 'stats', component: StatsComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "404", component: NotFoundComponent },
+  {
+    path: "car-hall",
+    component: ReeentACarComponent
+  },
+  { path: "stats", component: StatsComponent },
   {path: 'login', component: LoginPageComponent},
+  { path: "**", redirectTo: "/404", pathMatch: "full" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
