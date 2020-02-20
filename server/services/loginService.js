e74b16dbf5cc8907ea44fbd1a36afdb050bf5164
danielTiringer <tiringerdaniel@gmail.com>
2020-02-20 15:33:32 +0100

    feat(auth): implement bcrypt and hash password on reg. Compare decrypted when login

--- server/services/loginService.js
+++ server/services/loginService.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 class LoginService {
   constructor(conn, registrationService, generateAccessToken, generateRefreshToken) {
     this.conn = conn;
@@ -9,11 +11,11 @@ class LoginService {
   async authorizeUser(input) {
     const userInfo = await this.registrationService.containsUser(input);
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       if (userInfo.length === 0) return reject(new Error(400));
       if (userInfo[0].username == input.username) {
 				// Compare password
-				bcrypt.compare(input.password, userInfo[0].password)
+				await bcrypt.compare(input.password, userInfo[0].password)
 				.then(isMatch => {
 					if (isMatch) {
 						// Correct password, sending json token
--- server/services/registrationService.js
+++ server/services/registrationService.js
@@ -3,11 +3,11 @@ const bcrypt = require('bcryptjs');
 class RegistrationService {
   constructor(conn) {
     this.conn = conn;
+		this.hashPassword = this.hashPassword.bind(this)
   }
 
   checkIfUserNameNumLatinLetters(input) { // eslint-disable-line
-    const user = /^[A-Za-z0-9]\w{0,}$/;
-
+    const user = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
     if (input && user.test(input)) return true;
     return false;
   }
@@ -24,21 +24,23 @@ class RegistrationService {
       const query = 'SELECT * FROM users WHERE username = ?;';
 
       this.conn.query(query, [item.username], (err, row) => {
-				console.log(err)
         err ? reject(new Error(500)) : resolve(row);
       });
     });
   }
 
 	hashPassword(item) {
+		return new Promise((resolve, reject) => {
+
 		// Hash password
 		bcrypt.genSalt(10, (err, salt) => {
 			bcrypt.hash(item.password, salt, (err, hash) => {
 				if (err) { throw err; }
 				item.password = hash;
+				resolve(item);
 			})
 		})
-		return item;
+		})
 	}
 
   insertUser(item) {
@@ -53,13 +55,13 @@ class RegistrationService {
 
   async createUser(item) {
     const userData = await this.containsUser(item);
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       if (!this.checkIfUserNameNumLatinLetters(item.username) || !this.checkIfPasswordNumLatinLetter(item.password)) {
         reject(new Error(400));
       } else if (item.password !== item.confirmPsw) {
         reject(new Error(400));
       } else if (!userData.length) {
-				item = this.hashPassword(item);
+				item = await this.hashPassword(item)
         resolve(this.insertUser(item));
       } else {
         reject(new Error(500));
