3b2c9749cc88a839233cdf0db09f11fe775e5930
danielTiringer <53534182+danielTiringer@users.noreply.github.com>
2020-02-21 04:25:34 +0100

    style(reservation): add alert to reservation and reformat css (#28)

--- client/src/app/components/userinventory/userinventory.component.ts
+++ client/src/app/components/userinventory/userinventory.component.ts
@@ -2,6 +2,7 @@ import { Observable, Subscription } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserinventoryService } from 'src/app/services/userinventory/userinventory.service';
 import { Car } from 'src/app/models/car';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-userinventory',
@@ -46,6 +47,12 @@ export class UserInventoryComponent implements OnInit {
         setTimeout(() => {
           this.bookingResponse = 'Thank you, your booking has been made, you will receive an email confirmation if I can implement it in the next 6 hours'
         }, 1000);
+				Swal.fire({
+					icon: 'success',
+					title: 'Vehicle successfully reserved.',
+					showConfirmButton: false,
+					timer: 3500
+				})
       }, error => {
         console.log(error);
       });
